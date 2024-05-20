5fcc3a19ff273ff72c8e12d922bfdece04bed9d1
L-a-z-e <yysi8771@gmail.com>
2024-05-21 00:15:55 +0900

    mongoose crud api

--- chapter6/test-mongoose/mongoose-crud.js
+++ chapter6/test-mongoose/mongoose-crud.js
@@ -1,29 +1,33 @@
-const dotenvPath = require('path').resolve(__dirname, '../.env');
-console.log(`Trying to load .env from: ${dotenvPath}`);
-require('dotenv').config({ path: dotenvPath });
-// require('dotenv').config({ path: '../.env' });
+const dotenvPath = require('path').resolve(__dirname, '../../chapter6/.env');
+console.log(dotenvPath)
+require('dotenv').config({ path: '../../chapter6/.env' });
+console.log(process.env.MONGO_URI)
+const mongodbUri = process.env.MONGO_URI
 
-
-// require('dotenv').config({path: './chapter6/.env'});
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Person = require("./person-model.js");
 
+// 몽구스에서 쿼리에 필터를 빈 객체인 {}로 넣으면 모든 값을 불러오게 되어서 문제가 되는 경우
+// 이 경우에 에러를 내도록 하는 설정이 strictQuery
 mongoose.set("strictQuery", false);
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // HTTP에서 Body를 Parsing 하기위한 설정
 app.listen(3000, async () => {
     console.log('Server Started');
-    const mongodbUri = process.env.MONGODB_URI;
-    console.log(mongodbUri);
+
     mongoose.connect(mongodbUri)
         .then(console.log("Connected to MongoDB"))
         .catch(err => console.log(err));
 });
 
 // 모든 person 데이터 출력
+// 특정 데이터를 찾고싶으면 find{name:'Laze'} 로 찾을 수 있음 find(filter, projection, option)
+// filter - 검색값
+// projection - 가져올 데이터필드
+// option - sort(1 asc, -1 desc) { sort: {name:1}}, limit 결과 문서 수, skip - 앞에서 제외할 문서 수
 app.get("/person", async(req, res) => {
     const person = await Person.find({});
     res.send(person);
@@ -36,6 +40,7 @@ app.get("/person/:email", async (req, res) => {
 })
 
 // person 데이터 추가하기
+// new -> save || const result = await Person.create(req.body); res.send(result); 도 동일함
 app.post("/person", async(req, res) => {
     const person = new Person(req.body);
     await person.save();
@@ -49,6 +54,12 @@ app.put("/person/:email", async(req, res) => {
         { $set: req.body },
         { new: true }
     );
+
+    if (person) {
+        res.send(person);
+    } else {
+        res.status(404).send({message: 'Not Found'});
+    }
 });
 
 // person 데이터 삭제하기
